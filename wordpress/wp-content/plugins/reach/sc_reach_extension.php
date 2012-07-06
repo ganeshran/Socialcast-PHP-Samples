aa95f097d856334b588c3a571f490b59e8c48fc5
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2012-07-07 01:10:54 +0530

    Started development to make this plugin work with WP 3.4.1

--- wordpress/wp-content/plugins/reach/sc_reach_extension.php
+++ wordpress/wp-content/plugins/reach/sc_reach_extension.php
@@ -17,6 +17,8 @@
  * | along with this program; if not, write to the Free Software              |
  * | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA |
  * +--------------------------------------------------------------------------+
+ *
+ * Ganesh Ranganathan - Forked this plugin in an attempt to make it work with WP 3.4.1
  */
 
 /**
