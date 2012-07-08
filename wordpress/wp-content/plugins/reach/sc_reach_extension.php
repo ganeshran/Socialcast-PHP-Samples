63c4b0d4af6ec0c19545af72f249e128271b18b2
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2012-07-08 20:16:43 +0530

    new plugin options
    added options to enable/disable shortcode

--- wordpress/wp-content/plugins/reach/sc_reach_extension.php
+++ wordpress/wp-content/plugins/reach/sc_reach_extension.php
@@ -58,10 +58,24 @@ function get_div_email($email, $id, $style, $token) {
 }
 
 function sc_reach_content_handle($content, $sidebar = false) {
+	
+	switch(get_option('sc_show_button'))
+	{
+		case 'Dont Show':
+			break;
+		case 'Top':
+			$content =  sc_add_button() . $content;
+			break;
+		case 'Bottom':
+			$content = $content . sc_add_button();
+			break;
+	}
+	
 	if (get_option('sc_use_microdata') == 'true') {
 		$purl = get_permalink();
-   		return "<div itemscope=itemscope itemtype=\"http://ogp.me/ns#Blog\"><a itemprop='url' href='" . $purl . "' /></a>" . $content . "</div>";
+   		$content = "<div itemscope=itemscope itemtype=\"http://ogp.me/ns#Blog\"><a itemprop='url' href='" . $purl . "' /></a>" . $content . "</div>";
 	}
+
 	return $content;
 }
 
@@ -82,8 +96,8 @@ This function has been added to generate the div from the shortcut
 which can be added to any text widget or in the post itself. It includes
 an additional parameter - display which lets users insert shortcodes without
 knowing the token as long as they are specified in the Plugin options screen.
-Please make sure they are do_shortcode is called in your theme if you want to include 
-this in a widget */
+Please make sure they are do_shortcode is called in your theme for widget text if 
+you want to include this in a widget */
 function get_shortcode_div($id,$style,$token,$display){
  $tokenInOptions ='';
   if($display != '')
@@ -98,6 +112,9 @@ function get_shortcode_div($id,$style,$token,$display){
 				break;
 			case 'profile':
 			    $tokenInOptions = get_option('sc_profile_token');
+				break;
+			case 'trends':
+				$tokenInOptions = get_option('sc_trends_token');
 				break;
 				
 		}
@@ -135,11 +152,17 @@ function reach_init_method() {
   add_option('sc_discussion_token', '');
   add_option('sc_profile_token', '');
   add_option('sc_use_microdata', 'true');
-
+  add_option('sc_trends_token', '');
+  add_option('sc_enableShortcode','1');
+  add_option('sc_show_button','Dont Show');
   add_action('wp_head', 'sc_reach_header_meta');
   add_action('wp_footer', 'sc_reach_add_js');
-
+  if(get_option('sc_enableShortcode'))
   add_shortcode( 'reach', 'add_reach' );
+  else
+  remove_shortcode('reach');
+  
+//echo "Get option" . get_option('sc_show_button');
 
 }
 
@@ -151,16 +174,23 @@ function sc_reach_register_settings() {
   register_setting('sc_reach', 'sc_host');
   register_setting('sc_reach', 'sc_button_token');
   register_setting('sc_reach', 'sc_discussion_token');
+  register_setting('sc_reach', 'sc_trends_token');
   register_setting('sc_reach', 'sc_profile_token');
   register_setting('sc_reach', 'sc_use_microdata');
+  register_setting('sc_reach', 'sc_enableShortcode');
+  register_setting('sc_reach','sc_show_button');
 }
 
-function sc_add_button($style='width:300px;height:60px') {
+function sc_add_button($style='width:300px;height:30px') {
 	return get_div('like_container_id', $style, get_option('sc_button_token'));
 }
 
 function sc_add_discussion($style='width:300px;height:400px') {
 	return get_div('discussion_container_id', $style, get_option('sc_discussion_token'));
+}
+
+function sc_add_token($style='width:300px;height:400px'){
+	return get_div('trends_container_id',$style,get_option('sc_trends_token'));
 }
 
 function sc_reach_admin_menu() {
@@ -175,6 +205,7 @@ function sc_reach_options() {
 
     <form method="post" action="options.php">
     <?php
+	
     if (sc_reach_get_wp_version() < 2.7) {
       wp_nonce_field('update-options');
     } else {
@@ -193,6 +224,14 @@ function sc_reach_options() {
 			<tr><td>Button Token:</td><td><input style="width:400px" type="text" name="sc_button_token" value="<?php echo get_option('sc_button_token'); ?>" />Function: sc_add_button()</td></tr>
 			<tr><td>Discussion Token:</td><td><input style="width:400px" type="text" name="sc_discussion_token" value="<?php echo get_option('sc_discussion_token'); ?>" />Function sc_add_discussion()</td></tr>
             <tr><td>Profile Token:</td><td><input style="width:400px" type="text" name="sc_profile_token" value="<?php echo get_option('sc_profile_token'); ?>" />Function sc_add_author_stream()</td></tr>
+			<tr><td>Trends Token:</td><td><input style="width:400px" type="text" name="sc_trends_token" value="<?php echo get_option('sc_trends_token'); ?>" />Function sc_add_trends()</td></tr>
+			<tr><td>Enable Shortcode: </td><td><input name="sc_enableShortcode" type="checkbox" value="1" <?php checked( '1', get_option( 'sc_enableShortcode' ) ); ?> /></td></tr>
+			<tr><td>Show Button:</td> <td>
+			<select style="width:400px" name="sc_show_button" id="sc_show_button">
+<option value="Top" <?php if (get_option('sc_show_button')=='Top') echo 'selected="selected"';?>>Top</options>
+<option value="Bottom" <?php if (get_option('sc_show_button')=='Bottom') echo 'selected="selected"';?>>Bottom</options>
+<option value="Dont Show" <?php if (get_option('sc_show_button')=='Dont Show') echo 'selected="selected"';?>>Dont Show</options>
+</select>Make sure the button token is set for this to work</td></tr>
 		</table>
     <?php if (sc_reach_get_wp_version() < 2.7) : ?>
       <input type="hidden" name="action" value="update" />
